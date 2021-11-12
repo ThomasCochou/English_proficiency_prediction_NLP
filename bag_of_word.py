593b2cd0052ecdacfb133314c74a128de0ff391b
ThomasCochou <cochouthomas@gmail.com>
2021-11-12 15:17:43 +0100

    trying to create a Xtrain matric (not working)

--- bag_of_word.py
+++ bag_of_word.py
@@ -1,5 +1,6 @@
 import os
 from collections import Counter
+from keras.preprocessing.text import Tokenizer
 
 path = "preprocessed_text"
 
@@ -7,9 +8,14 @@ path = "preprocessed_text"
 MIN_OCCURANE = 2
 output_name = "vocab"
 
+
+##################################
+#   VOCABULARY
+##################################
 os.chdir(path)
 
 vocab = Counter()
+docs = list()
 
 for input_file in os.listdir():
     if input_file.endswith(".txt"):
@@ -18,11 +24,16 @@ for input_file in os.listdir():
         input_text.close()
 
         tokens = input_string.split()
-
         vocab.update(tokens)
 
-print("The 50 most common words: "+str(vocab.most_common(50)))
+        tokens = [w for w in tokens if w in vocab]
+        line = ' '.join(tokens)
+        docs.append(line)
 
+
+# print("The 50 most common words: "+str(vocab.most_common(50)))
+
+print(docs)
 
 tokens = [k for k,c in vocab.items() if c >= MIN_OCCURANE]
 
@@ -30,3 +41,11 @@ data = ' '.join(tokens)
 output_file = open("../"+output_name+".txt",'w')
 output_file.write(data)
 output_file.close()
+
+##################################
+#   DATASET
+##################################
+
+tokenizer = Tokenizer()
+Xtrain = tokenizer.texts_to_matrix(docs, mode='freq')
+print(Xtrain.shape)
