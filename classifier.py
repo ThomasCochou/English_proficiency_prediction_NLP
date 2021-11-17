381b4980849f68bbafd90de01dff2a9a0013355f
BRAND Lucas <l6brand@enib.fr>
2021-11-17 22:27:53 +0100

    commit pre-rebase

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 bag_of_word = ['my', 'name', 'is', 'xxx', 'hello', 'um', 'im', 'fine', 'but', 'a', 'little', 'nervous', 'laughter']
 
-from bag_of_word import vocab
+from bag_of_word import vocab, tokens
 
+print(tokens)
