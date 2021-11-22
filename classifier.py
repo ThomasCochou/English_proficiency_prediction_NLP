e4acc59041a49475981e73641417d0d2989f3c0b
BRAND Lucas <l6brand@enib.fr>
2021-11-22 09:48:23 +0100

    added neural network in notebook

--- classifier.py
+++ classifier.py
@@ -1,5 +1,7 @@
-bag_of_word = ['my', 'name', 'is', 'xxx', 'hello', 'um', 'im', 'fine', 'but', 'a', 'little', 'nervous', 'laughter']
+import os, pathlib, shutil, random
+from tensorflow import keras
 
-from bag_of_word import vocab, tokens
 
-print(tokens)
+train_data_matrix_path = "preprocessed_text/matrix_train_data/"
+test_data_matrix_path = "preprocessed_text/matrix_test_data/"
+
