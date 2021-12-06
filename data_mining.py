ee3ee4383a2f2279a9523371c7aeb2fead429db6
ThomasCochou <cochouthomas@gmail.com>
2021-12-06 13:37:29 +0100

    glove false

--- data_mining.py
+++ data_mining.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from nltk.corpus import words
 
-# Spell run python embedding.py -t cpu -m uploads/preprocessed_text -m uploads/glove
+# Spell run python embedding.py -t cpu -m uploads/preprocessed_text
 
 ##################################
 #   GOALS
